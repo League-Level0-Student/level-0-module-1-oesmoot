53ec1c3ea79555850d5e027c5a647e28218c9a39
root <root@970c4ca63b2e>
2023-03-26 19:54:12 +0000

    E

--- src/_03_if_else/_2_remarkable/Remarkable.java
+++ src/_03_if_else/_2_remarkable/Remarkable.java
@@ -9,11 +9,24 @@ public static void main(String[] args) {
 
 		// 1. Save something remarkable about each person in a variable.
 		String owen = "gamer";
-				
+		String nick = "programmer";
+		String mike = "programmer";
+		String dibideh = "random human";
 		// 2. Ask the user to enter a name. Store their answer in a variable.
-
+		String name = JOptionPane.showInputDialog(null,"enter a name");
 		// 3. In a pop-up, tell the user what is remarkable about that person. 
-
+		if (name.equals ("owen")) {
+			JOptionPane.showMessageDialog(null, owen);
+		}
+		if (name.equals("nick")) {
+			JOptionPane.showMessageDialog(null, nick);
+		}
+		if (name.equals("mike")) {
+			JOptionPane.showMessageDialog(null, mike);
+		}
+		if (name.equals("dibideh")) {
+			JOptionPane.showMessageDialog(null, dibideh);
+		}
 	}
 }
 
