95a3b427cb2484ab17428ae07aca22f37962f8a8
league <league@imac-34.attlocal.net>
2023-04-08 11:25:30 -0700

    happi

--- src/_03_if_else/_5_pizza/pizza/pizza.pde
+++ src/_03_if_else/_5_pizza/pizza/pizza.pde
@@ -1,7 +1,24 @@
 
 void setup() {
-    
+    size (600,600);
+    pepperoni = loadImage ("pepperoni.png");
+    olive = loadImage ("olive.png");
+    mushroom = loadImage ("mushroom.png");
 }
 void draw() {
-    
+  noStroke();
+   fill (250,229,121);
+   ellipse (300,300,400,400);
+   fill (250,0,0);
+   ellipse (300,300,350,350);
+   fill (255,255,0);
+   ellipse (300,300,335,335);
+   image (pepperoni,300,300);
+   olive.resize(40,40);
+   image (olive,200,300);
+   image (pepperoni,150,210);
+   image (mushroom,250,250);
 }
+PImage pepperoni;
+PImage olive;
+PImage mushroom;
--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -1,4 +1,7 @@
 package _04_int._1_riddler;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
@@ -9,13 +12,18 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+		int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+		String riddle1 = JOptionPane.showInputDialog("what tastes better than it smells?");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+		if (riddle1.equals("a tounge")) {
+			JOptionPane.showMessageDialog(null, "right!");
+			score ++;
+		}
 		// 5. Otherwise, say "wrong" and tell them the answer
-
+		else {
+			JOptionPane.showMessageDialog(null, "WRONG! the answer is: a tounge");
+		}
 		// 6. Add some more riddles
 
 		// 2. Make a pop up to show the score.
